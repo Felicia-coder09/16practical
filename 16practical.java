6bd5ff1714db8b89d62293f583888449c99aed8e
Kamogelo Felicia Mokwatedi <kamogelofelicia638@gmail.com>
2026-03-16 14:28:24 +0200

    Minimum Heap

--- 16practical.java
+++ 16practical.java
@@ -3,5 +3,12 @@ public class tryHeapsort {
 
   static String[] heap;
   static int size;
-  
+
+  //Swap
+  static void swap(int i, int j) {
+    String temp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = temp;
+  }
+  //Minimum heap
 }
